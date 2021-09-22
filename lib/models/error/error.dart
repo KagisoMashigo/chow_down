88d5e096b258f8dce8d0221b10013078fbddd555
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-09-22 14:27:50 -0400

    add models

--- lib/models/error/error.dart
+++ lib/models/error/error.dart
@@ -56,3 +56,12 @@ class ApiException implements Exception {
         url: url,
       );
 }
+
+class Failure {
+  final int code;
+  final String message;
+  Failure({
+    this.code,
+    this.message,
+  });
+}
