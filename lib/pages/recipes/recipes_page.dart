5c11aea174d1a1568f4e5dbaa22f3e94e6cdadc7
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-09-25 14:26:12 -0400

    recipe page WIP

--- lib/pages/recipes/recipes_page.dart
+++ lib/pages/recipes/recipes_page.dart
@@ -1,1 +1,34 @@
+import 'package:chow_down/core/data/models/spoonacular_models.dart/recipe.dart';
+import 'package:chow_down/services/spoonacular_api/recipe_endpoints.dart';
+import 'package:flutter/material.dart';
 
+class RecipePage extends StatefulWidget {
+  @override
+  _RecipePageState createState() => _RecipePageState();
+}
+
+class _RecipePageState extends State<RecipePage> {
+  Future<Recipe> service;
+  Recipe recipe;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    service = RecipeInformationService().getRecipe();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipes'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+        ],
+      ),
+    );
+  }
+}
