38579a400c501c21a8928874bf72d7005bb76416
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-09-30 16:27:20 -0400

    add multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:chow_down/providers/providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:chow_down/pages/landing_page.dart';
-import 'package:chow_down/services/auth.dart';
+// import 'package:chow_down/services/auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,8 +17,8 @@ Future<void> main() async {
 class ChowDown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider<AuthBase>(
-      create: (context) => Auth(),
+    return MultiProvider(
+      providers: providers,
       child: MaterialApp(
         title: 'Chow Down',
         theme: ThemeData(
