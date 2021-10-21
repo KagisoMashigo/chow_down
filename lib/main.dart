fd79fff348506ccbab396e06ce2e916075209b6e
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-10-21 15:04:48 -0400

    add providers

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:chow_down/providers/providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'package:chow_down/pages/landing_page.dart';
+import 'package:provider/provider.dart';
 // import 'package:chow_down/services/auth.dart';
 
 Future<void> main() async {
