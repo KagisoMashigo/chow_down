5098c611137c6853722923af84af0f8cbc15ae44
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-10-22 16:31:01 -0400

    acc page revamp

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -1,3 +1,4 @@
+import 'package:chow_down/plugins/responsive.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -29,20 +30,23 @@ class AccountPage extends StatelessWidget {
   }
 
   Widget _buildUserInfo(User user) {
-    return Column(
-      children: <Widget>[
-        Avatar(
-          radius: 50,
-          photoUrl: user.photoURL,
-        ),
-        SizedBox(height: 8),
-        if (user.displayName != null)
-          Text(
-            user.displayName,
-            style: TextStyle(color: Colors.white),
+    return Center(
+      child: Column(
+        children: [
+          verticalDivider(factor: 5),
+          Avatar(
+            radius: 50,
+            photoUrl: user.photoURL,
           ),
-        SizedBox(height: 8),
-      ],
+          verticalDivider(factor: 2),
+          if (user.displayName != null)
+            Text(
+              user.displayName,
+              style: TextStyle(color: Colors.white),
+            ),
+          verticalDivider(factor: 9),
+        ],
+      ),
     );
   }
 
@@ -50,8 +54,11 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthBase>(context, listen: false);
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text('Account'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        // title: Text('Account'),
         actions: <Widget>[
           TextButton(
             onPressed: () => _confirmSignOut(context),
@@ -64,9 +71,52 @@ class AccountPage extends StatelessWidget {
             ),
           ),
         ],
-        bottom: PreferredSize(
-          child: _buildUserInfo(auth.currentUser),
-          preferredSize: Size.fromHeight(130),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1609770231080-e321deccc34c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            // mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildUserInfo(auth.currentUser),
+              Text(
+                'Placeholder: options',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 2 * Responsive.ratioVertical),
+              ),
+              verticalDivider(factor: 4),
+              Text(
+                'Placeholder: more options? Lol',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 2 * Responsive.ratioVertical),
+              ),
+              verticalDivider(factor: 4),
+              Text(
+                'Placeholder: make these tiles',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 2 * Responsive.ratioVertical),
+              ),
+              verticalDivider(factor: 4),
+              Text(
+                'Placeholder: make logout more prominent',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 2 * Responsive.ratioVertical),
+              ),
+            ],
+          ),
         ),
       ),
     );
