b62290c6886f57f6a44465ef4e04776ca4dd6ca8
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-10-25 10:00:39 -0400

    clean code

--- lib/providers/authenticated_provider.dart
+++
@@ -1,7 +0,0 @@
-abstract class AuthenticatedProvider {
-  /// Called when the user has signed in
-  void init();
-
-  /// Called in case the user has signed out or session expired
-  void logout();
-}
--- lib/providers/provider_state.dart
+++
@@ -1,8 +0,0 @@
-/// The state of the provider
-enum ProviderStatus {
-  /// Signals the end of an Async call. Usually used at the end of an API call
-  done,
-
-  /// Signals the start of an ASYNC call. Usuallyused at the beggining of an API call
-  loading,
-}
