365d2a43466367fc963b174d87aeda0c49c11ae5
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-10-28 12:01:30 -0400

    clean main

--- lib/main.dart
+++ lib/main.dart
@@ -32,16 +32,16 @@ Future<void> main() async {
 // Main app call
 // TODO: Consider using cachedImage for network images
 class ChowDown extends StatelessWidget {
+  // TODO: make a theme file
   final primaryColor = Color(0xFF151026);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chow Down',
-
       theme: ThemeData(
         appBarTheme:
             AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.light),
-        // primarySwatch: primaryColor,
         fontFamily: 'Lato',
       ),
       // TODO: make sure logged in goes to home
