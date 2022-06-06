6f9e1b35e2513b6f63cde28845b0119cff7f2058
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2022-06-06 14:13:51 -0400

    custom tile

---
+++ lib/components/chow_list_tile.dart
@@ -0,0 +1,27 @@
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+
+class ChowListTile extends StatelessWidget {
+  const ChowListTile({
+    Key key,
+    @required this.title,
+    this.onTap,
+    this.leading,
+    this.trailing,
+  }) : super(key: key);
+
+  final Text title;
+  final Icon leading;
+  final Icon trailing;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: leading ?? Icon(Icons.data_exploration_outlined),
+      title: title,
+      onTap: onTap,
+      trailing: trailing ?? Icon(Icons.chevron_right_outlined),
+    );
+  }
+}
