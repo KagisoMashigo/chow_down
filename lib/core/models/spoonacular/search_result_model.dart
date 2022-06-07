3487b0a8e7a7fc574c193b68384c7fee6a1f235a
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2022-06-07 14:42:34 -0400

    add fields

--- lib/core/models/spoonacular/search_result_model.dart
+++ lib/core/models/spoonacular/search_result_model.dart
@@ -1,28 +1,34 @@
 class RecipeCardInfo {
   final int id;
-  final String name;
+  final String title;
   final String image;
   final String sourceUrl;
   final int readyInMinutes;
   final bool vegetarian;
+  final bool vegan;
+  final bool glutenFree;
   final int servings;
   RecipeCardInfo({
     this.id,
-    this.name,
+    this.title,
     this.image,
     this.sourceUrl,
     this.readyInMinutes,
     this.vegetarian,
+    this.glutenFree,
+    this.vegan,
     this.servings,
   });
   factory RecipeCardInfo.fromJson(Map<String, dynamic> json) {
     return RecipeCardInfo(
       id: json['id'],
-      name: json['title'],
+      title: json['title'],
       image: json['image'],
+      glutenFree: json['glutenFree'],
       sourceUrl: json['sourceUrl'],
       readyInMinutes: json['readyInMinutes'],
       vegetarian: json['vegetarian'],
+      vegan: json['vegan'],
       servings: json['servings'],
     );
   }
