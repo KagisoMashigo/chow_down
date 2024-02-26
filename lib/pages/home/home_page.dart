a1029aa3fc1c9175f93432bfcfcc08a4e0675de8
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2024-02-26 15:32:08 -0500

    make homepage stlss

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,164 @@
+// 🐦 Flutter imports:
+import 'dart:developer';
+
+import 'package:chow_down/components/forms/extract_recipe_form.dart';
+import 'package:chow_down/cubit/home_page/extract_bloc.dart';
+import 'package:chow_down/cubit/home_page/extract_event.dart';
+import 'package:flutter/material.dart';
+
+// 📦 Package imports:
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+// 🌎 Project imports:
+import 'package:chow_down/components/cards/expanded_help_card.dart';
+import 'package:chow_down/components/cards/recipe_card.dart';
+import 'package:chow_down/components/design/color.dart';
+import 'package:chow_down/components/design/responsive.dart';
+import 'package:chow_down/components/empty_content.dart';
+import 'package:chow_down/core/models/spoonacular/recipe_model.dart';
+import 'package:chow_down/pages/recipes/extracted_info_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: RefreshIndicator(
+          onRefresh: () => _pullRefresh(context),
+          child: SingleChildScrollView(
+            child: Container(
+              height: Responsive.isSmallScreen()
+                  ? MediaQuery.of(context).size.height
+                  : MediaQuery.of(context).size.height * 0.91,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: CachedNetworkImageProvider(
+                      'https://images.unsplash.com/photo-1558855410-3112e253d755?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGljZSUyMGNyZWFtfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=800&q=60'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              padding: EdgeInsets.only(top: 4 * Responsive.ratioVertical),
+              child: BlocConsumer<ExtractBloc, ExtractState>(
+                listener: (context, state) {
+                  if (state is ExtractError) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(state.message),
+                      ),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  log('Current state: $state');
+                  if (state is ExtractPending) {
+                    return _buildLoading();
+                  } else if (state is ExtractLoaded) {
+                    return _buildColumnWithData(state.extractedResult, context);
+                  } else {
+                    // error state snackbar
+                    return _buildInitialInput(context, state);
+                  }
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInitialInput(BuildContext context, ExtractState state) {
+    return Column(
+      children: [
+        verticalDivider(factor: 3.5),
+        Image.asset(
+          'assets/images/chow_down.png',
+          height: 18.5 * Responsive.ratioVertical,
+          width: 18.5 * Responsive.ratioVertical,
+          fit: BoxFit.cover,
+        ),
+        verticalDivider(factor: 5),
+        ExtractRecipeForm(),
+        verticalDivider(factor: 1),
+        HelpCard(),
+        if (state is! ExtractInitial)
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: 5 * Responsive.ratioHorizontal),
+            child: EmptyContent(
+              message: 'Try refreshing the page.',
+              title: 'Something went wrong...',
+              icon: Icons.error_outline_sharp,
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget _buildLoading() => Center(
+        child: CircularProgressIndicator(
+          color: ChowColors.white,
+        ),
+      );
+
+  Widget _buildColumnWithData(Recipe searchResult, BuildContext context) {
+    return Column(
+      children: [
+        verticalDivider(factor: 2),
+        Image.asset(
+          'assets/images/chow_down.png',
+          height: 18.5 * Responsive.ratioVertical,
+          width: 18.5 * Responsive.ratioVertical,
+          fit: BoxFit.fill,
+        ),
+        verticalDivider(factor: 2),
+        ExtractRecipeForm(),
+        verticalDivider(factor: 2),
+        HelpCard(),
+        verticalDivider(),
+        // This is the recipe card that is displayed after the user has submitted a URL
+        Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: 5 * Responsive.ratioHorizontal),
+          child: GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ExtractedInfoPage(
+                  title: searchResult.title,
+                  id: searchResult.id,
+                  sourceUrl: searchResult.sourceUrl,
+                ),
+              ),
+            ),
+            child: RecipeCard(
+              loadingColor: ChowColors.red700,
+              id: searchResult.id,
+              name: searchResult.title,
+              imageUrl: searchResult.image,
+              url: searchResult.sourceUrl!,
+              glutenFree: searchResult.glutenFree!,
+              readyInMinutes: searchResult.readyInMinutes!,
+              vegetarian: searchResult.vegetarian!,
+              vegan: searchResult.vegan!,
+              servings: searchResult.servings!,
+            ),
+          ),
+        ),
+        verticalDivider(factor: 4),
+      ],
+    );
+  }
+
+  Future<void> _pullRefresh(BuildContext context) async {
+    final extractCubit = context.read<ExtractBloc>();
+    extractCubit.add(Refresh());
+  }
+}
