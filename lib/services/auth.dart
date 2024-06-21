2da11be4ec96b369427fc653ba16408271db8976
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2024-06-20 22:11:38 -0400

    enhancment/31 add logging to auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,8 @@
 // 📦 Package imports:
+import 'package:chow_down/plugins/debugHelper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 abstract class AuthBase {
   User get currentUser;
@@ -25,65 +27,119 @@ class Auth implements AuthBase {
 
   @override
   Future<User> signInAnonymously() async {
-    final userCredentials = await _firebaseAuth.signInAnonymously();
-    return userCredentials.user!;
+    try {
+      printDebug('Attempting to sign in anonymously...');
+      final userCredentials = await _firebaseAuth.signInAnonymously();
+      printDebug(
+        'Anonymous sign-in successful: ${userCredentials.user!.uid}',
+        colour: DebugColour.green,
+      );
+      return userCredentials.user!;
+    } catch (e, stack) {
+      printAndLog(e, 'Anonymous sign-in failed, reason: $stack');
+      rethrow;
+    }
   }
 
   @override
   Future<User> signInWithEmailAndPassword(String email, String password) async {
-    final userCredentials = await _firebaseAuth.signInWithCredential(
-        EmailAuthProvider.credential(email: email, password: password));
-    return userCredentials.user!;
+    try {
+      printDebug('Attempting to sign in with email: $email');
+      final userCredentials = await _firebaseAuth.signInWithCredential(
+          EmailAuthProvider.credential(email: email, password: password));
+      printDebug('Email sign-in successful: ${userCredentials.user!.uid}');
+      return userCredentials.user!;
+    } catch (e, stack) {
+      printAndLog(e, 'Email sign-in failed: $stack');
+      rethrow;
+    }
   }
 
   @override
   Future<User> createUserWithEmailAndPassword(
       String email, String password) async {
-    final userCredentials = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
-    return userCredentials.user!;
+    try {
+      printDebug('Attempting to create user with email: $email');
+      final userCredentials = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      printDebug('User creation successful: ${userCredentials.user!.uid}');
+      return userCredentials.user!;
+    } catch (e, stack) {
+      printAndLog(e, 'User creation failed: $stack');
+      rethrow;
+    }
   }
 
-  // Reset Password
-  Future<void> sendPasswordResetEmail(String email) async =>
-      _firebaseAuth.sendPasswordResetEmail(email: email);
+  @override
+  Future<void> sendPasswordResetEmail(String email) async {
+    try {
+      printDebug('Attempting to send password reset email to: $email');
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+      printDebug('Password reset email sent to: $email');
+    } catch (e, stack) {
+      printAndLog(e, 'Password reset email failed: $stack');
+      rethrow;
+    }
+  }
 
   @override
   Future<User> signInWithGoogle() async {
-    final googleSignIn = GoogleSignIn();
-    final googleUser = await googleSignIn.signIn();
-    if (googleUser != null) {
-      final googleAuth = await googleUser.authentication;
-      if (googleAuth.idToken != null) {
-        final userCredential = await _firebaseAuth
-            .signInWithCredential(GoogleAuthProvider.credential(
-          idToken: googleAuth.idToken,
-          accessToken: googleAuth.accessToken,
-        ));
-        return userCredential.user!;
+    try {
+      printDebug('Attempting to sign in with Google...');
+      final googleSignIn = GoogleSignIn();
+      final googleUser = await googleSignIn.signIn();
+      if (googleUser != null) {
+        final googleAuth = await googleUser.authentication;
+        if (googleAuth.idToken != null) {
+          final userCredential = await _firebaseAuth
+              .signInWithCredential(GoogleAuthProvider.credential(
+            idToken: googleAuth.idToken,
+            accessToken: googleAuth.accessToken,
+          ));
+          printDebug('Google sign-in successful: ${userCredential.user!.uid}');
+          return userCredential.user!;
+        } else {
+          throw FirebaseAuthException(
+            message: 'Missing Google ID Token',
+            code: 'ERROR_MISSING_TOKEN',
+          );
+        }
       } else {
         throw FirebaseAuthException(
-          message: 'ERROR_MISSING_TOKEN',
-          code: 'Missing Google ID Token',
+          message: 'Sign in aborted by user',
+          code: 'ERROR_USER',
         );
       }
-    } else {
-      throw FirebaseAuthException(
-        message: 'Sign in aborted by user',
-        code: 'ERROR_USER',
-      );
+    } catch (e, stack) {
+      printAndLog(e, 'Google sign-in failed: $stack');
+      rethrow;
     }
   }
 
   @override
   Future<void> signOut() async {
-    final googleSignIn = GoogleSignIn();
-    await googleSignIn.signOut();
-    await _firebaseAuth.signOut();
+    try {
+      printDebug('Attempting to sign out...');
+      final googleSignIn = GoogleSignIn();
+      await googleSignIn.signOut();
+      await _firebaseAuth.signOut();
+      printDebug('Sign-out successful');
+    } catch (e, stack) {
+      printAndLog(e, 'Sign out failed: $stack');
+      rethrow;
+    }
   }
 
   @override
   Future<void> deleteUser() async {
-    await _firebaseAuth.currentUser?.delete();
+    try {
+      printDebug(
+          'Attempting to delete user: ${_firebaseAuth.currentUser?.uid}');
+      await _firebaseAuth.currentUser?.delete();
+      printDebug('User deletion successful: ${_firebaseAuth.currentUser?.uid}');
+    } catch (e, stack) {
+      printAndLog(e, 'User deletion failed: $stack');
+      rethrow;
+    }
   }
 }
