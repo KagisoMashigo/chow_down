45921f0c192d305c05eb5ae43afaea5a253bea44
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2024-06-20 22:41:51 -0400

    enhancment/31

--- lib/blocs/home_page/extract_bloc.dart
+++ lib/blocs/home_page/extract_bloc.dart
@@ -7,6 +7,7 @@ import 'package:chow_down/blocs/home_page/extract_state.dart';
 import 'package:chow_down/core/data/remotes/remote_spoonacular/recipe_remote_repository.dart';
 import 'package:chow_down/core/models/spoonacular/recipe_model.dart';
 import 'package:chow_down/models/error/error.dart';
+import 'package:chow_down/plugins/debugHelper.dart';
 import 'package:chow_down/plugins/utils/helpers.dart';
 
 class ExtractBloc extends Bloc<ExtractEvent, ExtractState> {
@@ -22,17 +23,27 @@ class ExtractBloc extends Bloc<ExtractEvent, ExtractState> {
     Emitter<ExtractState> emit,
   ) async {
     try {
+      printDebug('Attempting to extract recipe from URL: ${event.url}');
       emit(ExtractPending());
 
       if (StringHelper.isUrlValid(event.url)) {
         final Recipe extractedResult =
             await _recipeRepository.getExtractedRecipe(event.url);
 
+        printDebug('Recipe extraction successful from URL: ${event.url}');
+
         emit(ExtractLoaded(extractedResult));
       } else {
+        printDebug('Invalid or empty URL provided: ${event.url}');
+
         emit(ExtractError('The URL provided is invalid or empty', null));
       }
-    } on Failure catch (e) {
+    } on Failure catch (e, stack) {
+      printAndLog(
+        e,
+        'Recipe extraction failed for URL: ${event.url}, reason: $stack',
+      );
+
       emit(ExtractError(e.toString(), e.code));
     }
   }
@@ -42,12 +53,16 @@ class ExtractBloc extends Bloc<ExtractEvent, ExtractState> {
     Emitter<ExtractState> emit,
   ) async {
     try {
+      printDebug('Refreshing extract...');
       emit(ExtractPending());
 
-      Future.delayed(Duration(seconds: 2));
+      await Future.delayed(Duration(seconds: 2));
 
+      printDebug('Refresh complete, returning to initial state');
       emit(ExtractInitial());
-    } on Failure catch (e) {
+    } on Failure catch (e, stack) {
+      printAndLog(e, 'Refresh extract failed, reason: $stack');
+
       emit(ExtractError(e.toString(), e.code!));
     }
   }
