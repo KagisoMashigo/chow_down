09254f9bcdde6bd595048436d86dfd5b277cfb01
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2024-06-20 22:36:54 -0400

    enhancment/31 improve logging in recipe repo

--- lib/core/data/remotes/remote_spoonacular/recipe_remote_repository.dart
+++ lib/core/data/remotes/remote_spoonacular/recipe_remote_repository.dart
@@ -25,69 +25,58 @@ class RemoteRecipe implements RecipeRepository {
     receiveTimeout: RECEIVE_TIMEOUT_TIME,
   ));
 
+  @override
   Future<Recipe> getExistingRecipe(int id, String sourceUrl) async {
-    /// The first part of the function finds extracted recipes
-
     try {
       if (id == -1) {
+        printDebug('Attempting to extract recipe from URL: $sourceUrl');
         final endpoint =
             'https://api.spoonacular.com/recipes/extract?url=$sourceUrl&apiKey=$apiKey&analyze=true&forceExtraction=true&addRecipeInformation=true';
         final response = await dioClient.get(endpoint);
         final body = json.decode(response.toString());
+        printDebug('Recipe extraction successful from URL: $sourceUrl');
         return Recipe.fromJson(body);
       } else {
-        /// This part is for searched recipes
+        printDebug('Attempting to fetch recipe information with ID: $id');
         final endpoint =
             'https://api.spoonacular.com/recipes/$id/information?apiKey=$apiKey';
-
         final response = await dioClient.get(endpoint);
         final body = json.decode(response.toString());
+        printDebug('Recipe information fetched successfully with ID: $id');
         return Recipe.fromJson(body);
       }
-    } on SocketException catch (e) {
-      printDebug(e.message);
+    } on SocketException catch (e, stack) {
+      printAndLog(e, 'No Internet connection while fetching recipe: $stack');
       throw Failure(message: 'No Internet connection');
-    } on HttpException catch (e) {
-      printDebug(e.message);
+    } on HttpException catch (e, stack) {
+      printAndLog(e, 'HTTP error occurred while fetching recipe: $stack');
       throw Failure(message: 'There was a problem extracting the recipe');
-    } on DioException catch (e) {
-      printDebug(e.message!);
-      if (e.type == DioExceptionType.receiveTimeout) {
-        throw Failure(message: 'The request took too long to complete');
-      }
+    } on DioException catch (e, stack) {
+      printAndLog(e, 'Dio error occurred while fetching recipe: $stack');
       if (e.response?.statusCode == 503) {
-        printDebug(
-            'Error code: ${e.response?.statusCode}. In method getExistingRecipe');
-
+        printDebug('Server under maintenance: ${e.response?.statusCode}');
         throw Failure(
           message:
               'Looks like the server is under maintenance. Please try again later.',
           code: 503,
         );
       } else if (e.response?.statusCode == 400) {
-        printDebug(
-            'Error code: ${e.response?.statusCode}. In method getExistingRecipe');
-
+        printDebug('Invalid URL provided: ${e.response?.statusCode}');
         throw Failure(
           message: 'The URL provided is invalid or empty',
           code: 400,
         );
       } else if (e.response?.statusCode == 402) {
-        printDebug(
-            'Error code: ${e.response?.statusCode}. In method getExistingRecipe');
-
-        // TODO: add analytics event for this
-
+        printDebug('API quota exceeded: ${e.response?.statusCode}');
         throw Failure(
           message: 'API Quota exceeded. Please try again later.',
           code: 402,
         );
       } else {
-        printDebug(
-            'Error code: ${e.response?.statusCode}. In method getExistingRecipe');
-
+        printDebug('Error code: ${e.response?.statusCode}');
         throw Failure(
-          message: 'There was a problem fetching the recipe: ${e.message}',
+          message:
+              '[${e.response?.data['code']}]${e.response?.data['message']}',
           code: e.response?.statusCode,
         );
       }
@@ -100,44 +89,45 @@ class RemoteRecipe implements RecipeRepository {
         'https://api.spoonacular.com/recipes/extract?url=$url/&apiKey=$apiKey&analyze=true&forceExtraction=true&addRecipeInformation=true';
 
     try {
+      printDebug('Attempting to extract recipe from URL: $url');
       final response = await dioClient.get(endpoint);
       final body = json.decode(response.toString());
 
       if (response.data['analyzedInstructions'] != null &&
           response.data['extendedIngredients'] != null) {
+        printDebug('Recipe extraction successful from URL: $url');
         return Recipe.fromJson(body);
       } else {
         throw Failure(
             message:
                 'We were unable to extract the recipe from the provided URL');
       }
-    } on SocketException catch (e) {
-      printDebug(e.message);
+    } on SocketException catch (e, stack) {
+      printAndLog(e, 'No Internet connection while extracting recipe: $stack');
       throw Failure(message: 'No Internet connection');
-    } on HttpException catch (e) {
-      printDebug(e.message);
+    } on HttpException catch (e, stack) {
+      printAndLog(e, 'HTTP error occurred while extracting recipe: $stack');
       throw Failure(message: 'There was a problem extracting the recipe');
-    } on DioException catch (e) {
-      printDebug(e.message!);
+    } on DioException catch (e, stack) {
+      printAndLog(e, 'Dio error occurred while extracting recipe: $stack');
       if (e.type == DioExceptionType.receiveTimeout) {
         throw Failure(message: 'The request took too long to complete');
       }
       if (e.response?.statusCode == 503) {
+        printDebug('Server under maintenance: ${e.response?.statusCode}');
         throw Failure(
           message:
               'Looks like the server is under maintenance. Please try again later.',
           code: 503,
         );
       } else if (e.response?.statusCode == 400) {
-        printDebug('Error code: ${e.response?.statusCode}.');
-
+        printDebug('Invalid URL provided: ${e.response?.statusCode}');
         throw Failure(
           message: 'The URL provided is invalid or empty',
           code: 400,
         );
       } else {
-        printDebug('Error code: ${e.response?.statusCode}.');
-
+        printDebug('Error code: ${e.response?.statusCode}');
         throw Failure(
           message:
               'There was a problem extracting the recipe. Please try again.',
