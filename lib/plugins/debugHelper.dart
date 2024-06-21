0c64b0e68c33569cef2452a0d5627f6062076aa4
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2024-06-20 21:52:52 -0400

    enhancment/31 update debugger

--- lib/plugins/debugHelper.dart
+++ lib/plugins/debugHelper.dart
@@ -21,103 +21,65 @@ enum DebugColour {
 final FirebaseCrashlytics _crashlytics = FirebaseCrashlytics.instance;
 
 void printDebug(Object? object, {Type? type, DebugColour? colour}) {
-  if (kReleaseMode) {
+  if (kReleaseMode || object == null) {
     return;
   }
-  if (object == null) {
-    return;
-  }
+
+  final prefix = type != null ? '[$type] ' : '';
+  final message = '$prefix${object.toString()}';
 
   switch (colour) {
     case DebugColour.black:
-      printBlack((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[30m');
       break;
     case DebugColour.red:
-      printRed((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[31m');
       break;
     case DebugColour.green:
-      printGreen((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[32m');
       break;
     case DebugColour.yellow:
-      printYellow((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[33m');
       break;
     case DebugColour.blue:
-      printBlue((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[34m');
       break;
     case DebugColour.magenta:
-      printMagenta((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[35m');
       break;
     case DebugColour.cyan:
-      printCyan((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[36m');
       break;
     case DebugColour.white:
-      printWhite((type != null ? '[$type] ' : '') + object.toString());
+      _printWithColour(message, '\x1B[37m');
       break;
     default:
-      print((type != null ? '[$type] ' : '') + object.toString());
+      print(message);
       break;
   }
 }
 
-void printBlack(String value) {
-  printDebug('\x1B[30m$value\x1B[0m');
-}
-
-void printRed(String value) {
-  printDebug('\x1B[31m$value\x1B[0m');
-}
-
-void printGreen(String value) {
-  printDebug('\x1B[32m$value\x1B[0m');
-}
-
-void printYellow(String value) {
-  printDebug('\x1B[33m$value\x1B[0m');
-}
-
-void printBlue(String value) {
-  printDebug('\x1B[34m$value\x1B[0m');
-}
-
-void printMagenta(String value) {
-  printDebug('\x1B[35m$value\x1B[0m');
-}
-
-void printCyan(String value) {
-  printDebug('\x1B[36m$value\x1B[0m');
-}
-
-void printWhite(String value) {
-  printDebug('\x1B[37m$value\x1B[0m');
+void _printWithColour(String message, String colourCode) {
+  printDebug('$colourCode$message\x1B[0m');
 }
 
 void printAndLog(Object? object, String reason) {
   if (object is Exception) {
-    _crashlytics.recordError(
-      object,
-      StackTrace.current,
-      reason: reason,
-    );
+    _crashlytics.recordError(object, StackTrace.current, reason: reason);
   } else {
-    _crashlytics.recordError(
-      Exception(object),
-      StackTrace.current,
-      reason: reason,
-    );
+    _crashlytics.recordError(Exception(object), StackTrace.current,
+        reason: reason);
   }
 }
 
-/// Converts objects to JSON and pretty prints, if [object] is already JSON, it will
-/// be encoded on multiple lines with a space indent rather than a single line.
-///
-/// When [printInReleaseMode] is true, the JSON will be pretty printed in
-/// release builds.
 void prettyPrintJson(Object? object, {bool printInReleaseMode = false}) {
   try {
     final json = JsonEncoder.withIndent(' ').convert(object);
-    json.split('\n').forEach(
-          (line) => printInReleaseMode ? print(line) : printDebug(line),
-        );
+    for (var line in json.split('\n')) {
+      if (printInReleaseMode || !kReleaseMode) {
+        printDebug(line);
+      }
+    }
   } catch (e) {
     printDebug(e);
   }
