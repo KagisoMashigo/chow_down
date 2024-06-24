592b8474c452de06d41bd5ead8373e9da0b2ef4e
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2024-06-24 09:19:27 -0400

    enhancement/40 fix bugs from tests

--- lib/plugins/utils/helpers.dart
+++ lib/plugins/utils/helpers.dart
@@ -4,33 +4,18 @@ class StringHelper {
   /// Allows for proper formatting of times
   static String cookTimeConverter(int cookTime) {
     final duration = Duration(minutes: cookTime);
-    List<String> timeParts = duration.toString().split(':');
-    final hours = timeParts[1] == '00'
-        ? '${timeParts[0].padLeft(2, '')} hrs'
-        : '${timeParts[0].padLeft(2, '')} hrs ${timeParts[1].padLeft(2, '0')} mins';
+    final hours = duration.inHours;
+    final minutes = duration.inMinutes % 60;
 
-    final minutes = timeParts[1].startsWith('0')
-        ? '${timeParts[1].padLeft(2, '').split('0')[1]} mins'
-        : '${timeParts[1].padLeft(0, '')} mins';
-
-    final hour = '${timeParts[0].padLeft(2, '')} hour';
-
-    final oneHourAndMins =
-        '${timeParts[0].padLeft(2, '')} hr ${timeParts[1].padLeft(2, '0')} mins';
-
-    if (cookTime < 120 && cookTime > 60) {
-      return oneHourAndMins;
+    if (cookTime < 60) {
+      return '$minutes mins';
+    } else if (cookTime == 60) {
+      return '1 hour';
+    } else if (cookTime < 120) {
+      return '$hours hr $minutes mins';
+    } else {
+      return '$hours hrs ${minutes.toString().padLeft(2, '0')} mins';
     }
-
-    if (cookTime == 60) {
-      return hour;
-    }
-
-    if (cookTime >= 120) {
-      return hours;
-    }
-
-    return minutes;
   }
 
   static bool isUrlValid(String? url) {
