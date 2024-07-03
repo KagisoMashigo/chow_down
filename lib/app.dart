b8a191ca966d2b4de69c993394852bb587b1e3de
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2024-07-03 11:45:29 -0400

    enhancement/37 fix auth flow

--- lib/app.dart
+++ lib/app.dart
@@ -1,112 +1,70 @@
 // 🐦 Flutter imports:
-import 'package:chow_down/blocs/edit_recipe/edit_recipe_bloc.dart';
+import 'package:chow_down/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 // 📦 Package imports:
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 // 🌎 Project imports:
-import 'package:chow_down/blocs/home_page/extract_bloc.dart';
-import 'package:chow_down/blocs/recipe_info/recipe_detail_bloc.dart';
-import 'package:chow_down/blocs/saved_recipe/saved_recipe_bloc.dart';
-import 'package:chow_down/blocs/search/search_bloc.dart';
+
 import 'package:chow_down/components/design/color.dart';
 import 'package:chow_down/components/design/responsive.dart';
 import 'package:chow_down/components/design/typography.dart';
-import 'package:chow_down/core/data/remotes/remote_spoonacular/recipe_remote_repository.dart';
-import 'package:chow_down/core/data/remotes/remote_spoonacular/search_remote_repository.dart';
 import 'package:chow_down/pages/landing_page.dart';
-import 'package:chow_down/services/auth.dart';
-import 'package:chow_down/services/firestore/firestore_db.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final database = FirestoreDatabase(uid: Auth().currentUser.uid);
-
-    Widget widget = MultiBlocProvider(
-      providers: [
-        Provider<AuthBase>(
-          create: (_) => Auth(),
-        ),
-        BlocProvider<RecipeDetailBloc>(
-          create: (context) => RecipeDetailBloc(
-            RemoteRecipe(),
-            database,
-          ),
-        ),
-        BlocProvider<SavedRecipeBloc>(
-          create: (context) => SavedRecipeBloc(
-            database,
-          ),
-        ),
-        BlocProvider<SearchBloc>(
-          create: (context) => SearchBloc(
-            RemoteSearchRepository(),
-          ),
-        ),
-        BlocProvider<ExtractBloc>(
-          create: (context) => ExtractBloc(
-            RemoteRecipe(),
-          ),
-        ),
-        BlocProvider<EditRecipeBloc>(
-          create: (context) => EditRecipeBloc(
-            database,
-          ),
-        ),
-      ],
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return OrientationBuilder(
-            builder: (context, orientation) {
-              Responsive.init(constraints, orientation);
-              SystemChrome.setPreferredOrientations(
-                [
-                  DeviceOrientation.portraitUp,
-                  DeviceOrientation.portraitDown,
-                ],
-              );
-              SystemChrome.setSystemUIOverlayStyle(
-                SystemUiOverlayStyle(
-                  statusBarColor: Colors.transparent,
-                  statusBarIconBrightness: Brightness.dark,
-                  systemNavigationBarColor: Colors.white,
-                  systemNavigationBarIconBrightness: Brightness.dark,
-                ),
-              );
-              return ChowDown();
-            },
-          );
-        },
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            Responsive.init(constraints, orientation);
+            SystemChrome.setPreferredOrientations(
+              [
+                DeviceOrientation.portraitUp,
+                DeviceOrientation.portraitDown,
+              ],
+            );
+            SystemChrome.setSystemUIOverlayStyle(
+              SystemUiOverlayStyle(
+                statusBarColor: Colors.transparent,
+                statusBarIconBrightness: Brightness.dark,
+                systemNavigationBarColor: Colors.white,
+                systemNavigationBarIconBrightness: Brightness.dark,
+              ),
+            );
+            return ChowDown();
+          },
+        );
+      },
     );
-
-    return widget;
   }
 }
 
 class ChowDown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      color: ChowColors.black,
-      title: 'Chow Down',
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          systemOverlayStyle: SystemUiOverlayStyle.light,
+    return Provider<AuthBase>(
+      create: (context) => Auth(),
+      child: MaterialApp(
+        color: ChowColors.black,
+        title: 'Chow Down',
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            systemOverlayStyle: SystemUiOverlayStyle.light,
+          ),
+          fontFamily: ChowFontFamilies.primary,
         ),
-        fontFamily: ChowFontFamilies.primary,
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
+        ],
+        home: LandingPage(),
       ),
-      navigatorObservers: [
-        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
-      ],
-      home: LandingPage(),
     );
   }
 }
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,8 +1,17 @@
 // 🐦 Flutter imports:
+import 'package:chow_down/blocs/edit_recipe/edit_recipe_bloc.dart';
+import 'package:chow_down/blocs/home_page/extract_bloc.dart';
+import 'package:chow_down/blocs/recipe_info/recipe_detail_bloc.dart';
+import 'package:chow_down/blocs/saved_recipe/saved_recipe_bloc.dart';
+import 'package:chow_down/blocs/search/search_bloc.dart';
+import 'package:chow_down/components/design/chow.dart';
+import 'package:chow_down/core/data/remotes/remote_spoonacular/recipe_remote_repository.dart';
+import 'package:chow_down/core/data/remotes/remote_spoonacular/search_remote_repository.dart';
 import 'package:flutter/material.dart';
 
 // 📦 Package imports:
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 // 🌎 Project imports:
@@ -23,14 +32,48 @@ class LandingPage extends StatelessWidget {
           if (user == null) {
             return SignInPage.create(context);
           }
-          return Provider<Database>(
-            create: (_) => FirestoreDatabase(uid: user.uid),
-            child: TabManager(),
+          final database = FirestoreDatabase(uid: user.uid);
+
+          return MultiBlocProvider(
+            providers: [
+              BlocProvider<RecipeDetailBloc>(
+                create: (context) => RecipeDetailBloc(
+                  RemoteRecipe(),
+                  database,
+                ),
+              ),
+              BlocProvider<SavedRecipeBloc>(
+                create: (context) => SavedRecipeBloc(
+                  database,
+                ),
+              ),
+              BlocProvider<SearchBloc>(
+                create: (context) => SearchBloc(
+                  RemoteSearchRepository(),
+                ),
+              ),
+              BlocProvider<ExtractBloc>(
+                create: (context) => ExtractBloc(
+                  RemoteRecipe(),
+                ),
+              ),
+              BlocProvider<EditRecipeBloc>(
+                create: (context) => EditRecipeBloc(
+                  database,
+                ),
+              ),
+            ],
+            child: Provider<Database>(
+              create: (_) => FirestoreDatabase(uid: user.uid),
+              child: TabManager(),
+            ),
           );
         }
         return Scaffold(
           body: Center(
-            child: CircularProgressIndicator(),
+            child: CircularProgressIndicator(
+              color: ChowColors.white,
+            ),
           ),
         );
       },
